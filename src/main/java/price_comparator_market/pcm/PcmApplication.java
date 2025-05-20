41f7d853191be41442c2835f0900d27143544a93
radusrbu <163407419+radusrbu@users.noreply.github.com>
2025-05-20 20:38:24 +0300

    Conmfigured Spring Boot

---
+++ src/main/java/price_comparator_market/pcm/PcmApplication.java
@@ -0,0 +1,13 @@
+package price_comparator_market.pcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/price_comparator_market/pcm/PcmApplicationTests.java
@@ -0,0 +1,13 @@
+package price_comparator_market.pcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
